e47ec4adf1ae5e192eb5e433f5d54a40e93d8ad8
Saga Einarsdotter Kikajon <saga@kikajon.se>
2025-02-25 16:44:57 +0100

    Småjusteringar media queries

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//Anslut till databasen 
+//Anslut till databasen
 builder.Services.AddDbContext<BookAppContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("BookAppConnection"))
 );
